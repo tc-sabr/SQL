5f0612b68ce531cfb4149e1cda5cac533455c1f3
tc-sabr <42155277+tc-sabr@users.noreply.github.com>
2018-10-19 12:15:57 -0600

    part 2 answers

---
+++ Part_2.sql
@@ -0,0 +1,42 @@
+USE Sakila;
+
+-- Part 2a: find ID, first name, last name when only knowing first name is Joe
+SELECT
+	actor_id
+    , first_name
+    , last_name
+FROM
+	actor
+WHERE
+	first_name LIKE 'Joe'
+;
+
+-- Part 2b: find actors whose last name contains GEN
+SELECT
+	first_name
+    , last_name
+FROM
+	actor
+WHERE
+	last_name LIKE '%GEN%'
+;
+
+-- Part 2c: fins actors whose last name contains LI and order last name then first name
+SELECT
+	last_name
+    , first_name
+FROM
+	actor
+WHERE
+	last_name LIKE '%LI%'
+;
+
+-- Part 2d: use in to display id and country of Afghanistan, Bangladesh, and China
+SELECT
+	country_id
+    , country
+FROM
+	country
+WHERE 
+	country IN ('Afghanistan', 'Bangladesh', 'China')
+;
